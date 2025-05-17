c4116cd984e3cb2b830da263aa2bfad9d6697e54
Nadisa Yulianty <nadisayulianty@gmail.com>
2025-05-17 14:11:13 +0700

    crud mahasiswa (pertemuan 5)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MahasiswaModule } from './mahasiswa/mahasiswa.module';
 
 @Module({
-  imports: [],
+  imports: [MahasiswaModule],
   controllers: [AppController],
   providers: [AppService],
 })
