ce49572577075caae0812300faf8ee5047f18511
Nadisa Yulianty <nadisayulianty@gmail.com>
2025-06-12 22:21:06 +0700

    mysql integration with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MahasiswaModule } from './mahasiswa/mahasiswa.module';
-import { MataKuliahModule } from './mata-kuliah/mata-kuliah.module';
+import { Mahasiswa } from './mahasiswa/entities/mahasiswa.entity';
 
 @Module({
-  imports: [MahasiswaModule, MataKuliahModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '3306', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [Mahasiswa],
+      synchronize: true,
+    }),
+    MahasiswaModule,
+  ],
 })
 export class AppModule {}
